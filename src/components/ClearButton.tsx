9d801d5b08ae7795f1046afa99a5aa530da96677
Douya Qki <villarrealjoseluis23@gmail.com>
2025-05-12 14:14:38 -0300

    fix | button: edit & delete | hidden while cleared all & create a new todo.

--- src/components/ClearButton.tsx
+++ src/components/ClearButton.tsx
@@ -3,15 +3,22 @@ import useRedux from "../useRedux";
 
 // styles
 import { CLEAR_BUTTON_STYLE } from "../tailwindStyles";
+import { toggle } from "../store/display/displaySlice";
 
 function ClearButton() {
   const { todo, display, deleteAll, dispatch } = useRedux();
+  const DISPLAY = "";
+
 
   const editorMode = todo.some(({editorMode}) => editorMode !== true)
 
   const SOME_TODO = todo.length >= 1 && editorMode
 
-  const clear = () => dispatch(deleteAll());
+  const clear = () => {
+    dispatch(deleteAll())
+    dispatch(toggle(DISPLAY))
+  }
+
   const CLEAR = "Clear all";
   return (
     SOME_TODO && (
